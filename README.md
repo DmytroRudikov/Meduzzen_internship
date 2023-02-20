a34b7a030c17eb0e5aae248ed16ca0d114369814
Dmytro Rudikov <rudikov1@gmail.com>
2023-02-20 19:43:58 +0200

    Task #1 initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# FastAPI project
+
+## to run tests from Meduzzen_internship repository
+    
+    python -m pytest
+
+
+## to run the app from Meduzzen_internship repository
+
+    python app/main.py
--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
+import pytest
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def health_check():
+    return {"status_code": 200,
+            "detail": "ok",
+            "result": "working"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True)

b67b1f90ec3db7c5894af9beb312dccb3382afe0
Dmytro Rudikov <rudikov1@gmail.com>
2023-02-23 12:41:33 +0200

    Task #3 initial commit (for mentor to check current progress)

--- app/main.py
+++ app/main.py
@@ -21,8 +21,7 @@ app.add_middleware(
 async def health_check():
     return {"status_code": 200,
             "detail": "ok",
-            "result": "working",
-            "hi": "smth"}
+            "result": "working"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=os.getenv("HOST"), port=int(os.getenv("PORT")), reload=True)

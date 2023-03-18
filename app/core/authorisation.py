a3a437cbd615ae112ac637b722f164a764a5db65
Dmytro Rudikov <rudikov1@gmail.com>
2023-03-18 21:14:53 +0200

    Task #6: Виправлено відповідно до коментарів

--- app/core/authorisation.py
+++ app/core/authorisation.py
@@ -1,8 +1,10 @@
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from databases import Database
 from jose import jwt, JWTError
-from schemas import token_schemas, user_schemas
-from services.crud_user import context, UserCrud
+from app.schemas import token_schemas, user_schemas
+from app.services.crud_user import context, UserCrud
+from app.core.db_config import get_sql_db
 import datetime
 import os
 from dotenv import load_dotenv
@@ -26,7 +28,8 @@ class Auth:
     def verify_password(plain_password, hashed_password):
         return context.verify(plain_password, hashed_password)
 
-    async def authenticate_user(self, email: str, plain_password: str, crud_user: UserCrud) -> user_schemas.User:
+    async def authenticate_user(self, email: str, plain_password: str) -> user_schemas.User:
+        crud_user = UserCrud(db=get_sql_db())
         user = await crud_user.user_exists_for_auth(email=email)
         if not self.verify_password(plain_password=plain_password, hashed_password=user.password):
             raise self.credentials_exception
@@ -39,14 +42,15 @@ class Auth:
         encode_jwt = jwt.encode(claims=to_encode, key=self.SECRET_KEY, algorithm=self.ALGORITHM)
         return encode_jwt
 
-    async def get_token_at_signin(self, login_form: user_schemas.SigninRequest, crud_user: UserCrud) -> token_schemas.Token:
-        user = await self.authenticate_user(email=login_form.email, plain_password=login_form.password, crud_user=crud_user)
+    async def get_token_at_signin(self, login_form: user_schemas.SigninRequest) -> token_schemas.Token:
+        user = await self.authenticate_user(email=login_form.email, plain_password=login_form.password)
         expires_delta = datetime.timedelta(minutes=self.ACCESS_TOKEN_EXPIRE_MINUTES)
         data = {"user_email": user.email}
         access_token = self.create_token(data=data, expires_delta=expires_delta)
         return {"access_token": access_token, "token_type": "Bearer"}
 
-    async def get_current_user_email(self, token: str = Depends(oauth_scheme)) -> user_schemas.User:
+    async def get_current_user(self, token: str = Depends(oauth_scheme)) -> user_schemas.User:
+        crud_user = UserCrud(db=get_sql_db())
         try:
             payload = jwt.decode(token=token, key=self.SECRET_KEY, algorithms=self.ALGORITHM)
             user_email = payload.get("user_email")
@@ -55,4 +59,5 @@ class Auth:
         except JWTError:
             raise self.credentials_exception
         else:
-            return user_email
+            user = crud_user.user_exists_for_auth(email=user_email)
+            return user
--- app/routers/user_routers.py
+++ app/routers/user_routers.py
@@ -5,25 +5,17 @@ sys.path.append(os.getcwd())
 from databases import Database
 from app.schemas import user_schemas
 from fastapi import APIRouter, Depends
-from app.core.authorisation import Auth
 from typing import List
 from app.services.crud_user import UserCrud
 from app.core.db_config import get_sql_db
 
 router = APIRouter()
-auth = Auth()
 
 
 @router.get("/user", response_model=user_schemas.User)
 async def get_user(user_id: int, db: Database = Depends(get_sql_db)) -> user_schemas.User:
     crud_user = UserCrud(db=db)
     return await crud_user.get_user(user_id=user_id)
-
-
-@router.get("/auth/me", response_model=user_schemas.User)
-async def get_current_user(user_email: str = Depends(auth.get_current_user_email), db: Database = Depends(get_sql_db)) -> user_schemas.User:
-    crud_user = UserCrud(db=db)
-    return await crud_user.user_exists_for_auth(email=user_email)
 
 
 @router.get("/users", response_model=List[user_schemas.User])

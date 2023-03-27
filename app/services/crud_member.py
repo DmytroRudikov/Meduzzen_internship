729fa56e797e778d8ea063d77b95de18c188402b
Dmytro Rudikov <rudikov1@gmail.com>
2023-03-27 22:29:41 +0300

    Task #10: initial commit

--- app/services/crud_member.py
+++ app/services/crud_member.py
@@ -11,6 +11,12 @@ class MemberCrud:
     def __init__(self, db: Database):
         self.db = db
 
+    async def member_exists(self, company_member_id: int, company_id: int):
+        member_exists = await self.db.fetch_one(select(models.MemberRecord).filter_by(company_id=company_id, company_member_id=company_member_id))
+        if not member_exists:
+            raise HTTPException(status_code=404, detail=f"Company member with id {company_member_id} not found")
+        return member_exists
+
     async def check_the_last_company_member_id(self, company_id: int) -> int:
         last_company_member = await self.db.fetch_one(select(models.MemberRecord).filter_by(company_id=company_id).order_by(models.MemberRecord.company_member_id.desc()))
         if last_company_member:
@@ -44,8 +50,18 @@ class MemberCrud:
         result = await self.db.fetch_all(select(models.MemberRecord).filter_by(company_id=company_id))
         return result
 
-    async def update_member(self):
-        pass
+    async def get_all_admins(self, company_id: int) -> List[member_schemas.Member]:
+        result = await self.db.fetch_all(select(models.MemberRecord).filter_by(company_id=company_id, role="admin"))
+        return result
+
+    async def set_remove_admin(self, company_member_id: int, company_id: int) -> status.HTTP_200_OK:
+        member = await self.member_exists(company_member_id=company_member_id, company_id=company_id)
+        if member.role is None or member.role == "":
+            values = {"role": "admin"}
+        else:
+            values = {"role": None}
+        await self.db.execute(update(models.MemberRecord).filter_by(company_member_id=company_member_id, company_id=company_id).values(**values))
+        return HTTPException(status_code=200, detail="success")
 
     async def delete_member(self, company_id: int, company_member_id: int | None = None, user_id: int | None = None) -> status.HTTP_200_OK:
         if user_id is None:
